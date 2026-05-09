7ba0e7bcd3a9372608e2be0d40ab1c877472873c
Shayfeint <richmondbinuya6@gmail.com>
2026-05-09 18:00:33 +0800

    1st commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ADET_Group_12.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
